6861a0e73b48492f4ee71382032479bacf460fa6
silvanoamaral <silvano.amaral9@gmail.com>
2024-07-28 21:39:45 -0300

    Aplicando o get do servidor

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,16 @@
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
-  };
+  try {
+    // Aqui você pode chamar seu servidor Express, se necessário, usando fetch ou outro método
+    const response = await fetch('http://localhost:3000/api/hello');
+    const data = await response.text();
+    return {
+      statusCode: 200,
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+    };
+  }
 }; 
